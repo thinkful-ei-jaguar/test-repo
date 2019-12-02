eb98104a1700250d9d4863ff669ad545b3ac3623
Nghi Tran <nghi.t.tran8@gmail.com>
2019-12-02 14:07:20 -0800

    rock paper scissors

--- function-drills-2/index.js
+++ function-drills-2/index.js
@@ -9,7 +9,7 @@ console.log(jediName('nghi', 'tran'));
 
 function beyond(num) {
   if (Number.isFinite(num) && num > 0) {
-    console.log("To infinity");
+    console.log('To infinity');
   } else if (Number.isFinite(num) && num < 0) {
     console.log('To negative inifinity');
   } else if (num === 0) {
@@ -23,60 +23,93 @@ function decode(encodedWord) {
 
   let char;
   switch (encodedWord[0]) {
-    case 'a':
-      char = encodedWord[1];
-      break;
-    case 'b':
-      char = encodedWord[2];
-      break;
-    case 'c':
-      char = encodedWord[3];
-      break;
-    case 'd':
-      char = encodedWord[4];
-      break;
-    default:
-      char = ' ';
-      break;
+  case 'a':
+    char = encodedWord[1];
+    break;
+  case 'b':
+    char = encodedWord[2];
+    break;
+  case 'c':
+    char = encodedWord[3];
+    break;
+  case 'd':
+    char = encodedWord[4];
+    break;
+  default:
+    char = ' ';
+    break;
   }
 
   return char;
 }
 
 let words = 'craft block argon meter bells brown croon droop';
-let listOfWords = words.split(" ");
+let listOfWords = words.split(' ');
 for (let i = 0; i < listOfWords.length; i++) {
   console.log(decode(listOfWords[i]));
 }
 
 function daysInMonth(month, leapYear = false) {
   switch (month) {
-    case 'January':
-    case 'March':
-    case 'May':
-    case 'July':
-    case 'August':
-    case 'October':
-    case 'December':
-      return `${month} has 31 days.`;
-      break;
-    case 'February': {
-      if (leapYear) {
-        return 'February has 29 days.';
-      } else {
-        return 'February has 28 days.';
-      }
-      break;
+  case 'January':
+  case 'March':
+  case 'May':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    return `${month} has 31 days.`;
+    break;
+  case 'February': {
+    if (leapYear) {
+      return 'February has 29 days.';
+    } else {
+      return 'February has 28 days.';
     }
-    case 'April':
-    case 'June':
-    case 'September':
-    case 'November':
-      return `${month} has 30 days.`;
-      break;
-    default:
-      return 'Must provide a valid month.';
+    break;
+  }
+  case 'April':
+  case 'June':
+  case 'September':
+  case 'November':
+    return `${month} has 30 days.`;
+    break;
+  default:
+    return 'Must provide a valid month.';
   }
 }
 
 console.log(daysInMonth('January'));
+
+
+function game(num) {
+  // 1 is rock
+  // 2 is paper
+  // 3 is scissors
+  if(num !== 1 && num && 2 && num !== 3) {
+    throw new Error('Not a valid number.');
+  }
+  const randomNo = Math.floor(Math.random() * 3) +1;
+  let result;
+
+  switch(num,randomNo){
+  case (1,3):
+  case (3,2):
+  case (2,1):
+    result = 'Player 1 wins';
+    break;
+  case (3,1):
+  case (2,3):
+  case (1,2):
+    result = 'Player 2 wins';
+    break;
+  case (1,1):
+  case (2,2):
+  case (3,3):
+    result = 'Its a tie!';
+    break;
+  }
+  return result;
+}
+
+console.log(game(3));
