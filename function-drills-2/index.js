f09bbabc569769aaed8cb8b50614f7630f04b723
Nghi Tran <nghi.t.tran8@gmail.com>
2019-12-02 13:38:51 -0800

    decoded words

--- function-drills-2/index.js
+++ function-drills-2/index.js
@@ -18,3 +18,33 @@ function beyond(num) {
     console.log('And beyond');
   }
 }
+
+function decode(encodedWord) {
+
+  let char;
+  switch(encodedWord[0]) {
+    case 'a':
+      char = encodedWord[1];
+      break;
+    case 'b':
+      char = encodedWord[2];
+      break;
+    case 'c':
+      char = encodedWord[3];
+      break;
+    case 'd':
+      char = encodedWord[4];
+      break;
+    default:
+      char = ' ';
+      break;
+  }
+
+  return char;
+}
+
+let words = 'craft block argon meter bells brown croon droop';
+let listOfWords = words.split(" ");
+for (let i=0; i<listOfWords.length; i++) {
+  console.log(decode(listOfWords[i]));
+}
