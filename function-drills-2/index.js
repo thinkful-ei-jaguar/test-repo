2bb9509db7e4ef9ed982904b2cb1d2141ec3093e
Brittany <brittjsauce@yahoo.com>
2019-12-02 15:23:22 -0600

    to inifity

--- function-drills-2/index.js
+++ function-drills-2/index.js
@@ -1,7 +1,20 @@
 /* eslint-disable strict */
-function jediName(firstName, lastName){
+function jediName(firstName, lastName) {
   let jediN;
-  return lastName.slice(0,3) + firstName.slice(0,2);
+  return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
 
-console.log(jediName('nghi','tran'));
+console.log(jediName('nghi', 'tran'));
+
+
+function beyond(num) {
+  if (Number.isFinite(num) && num > 0) {
+    console.log("To infinity");
+  } else if (Number.isFinite(num) && num < 0) {
+    console.log('To negative inifinity');
+  } else if (num === 0) {
+    console.log('Staying home.');
+  } else {
+    console.log('And beyond');
+  }
+}
