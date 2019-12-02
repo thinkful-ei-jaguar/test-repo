d4e24bf31c33191cef3cb32baf8685f38d84a4ba
Brittany <brittjsauce@yahoo.com>
2019-12-02 14:49:09 -0600

    Added numbers 6 & 7

--- function-drills-1/index.js
+++ function-drills-1/index.js
@@ -1,10 +1,23 @@
 /* eslint-disable strict */
 
-funtion getYearOfBirth(age) {
-    return 2019 - age;
+function getYearOfBirth(age) {
+  if (age < 0) {
+    throw new Error('Age cannot be negative.');
+  }
+  return 2019 - age;
 }
 
+try {
+  const greet = createGreeting('Brittany', -3);
+} catch (e) {
+  console.log(e);
+};
+
 function createGreeting(name, age) {
+  if (!name || !age) {
+    throw new Error('Arguments not valid.');
+  }
+
   const yob = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
 }
