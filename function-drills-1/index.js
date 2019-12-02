d5d15d4dfc1365030b40e378b60d6de969037517
Nghi Tran <nghi.t.tran8@gmail.com>
2019-12-02 12:55:36 -0800

    changes added

--- function-drills-1/index.js
+++ function-drills-1/index.js
@@ -1,19 +1,20 @@
 /* eslint-disable strict */
 
 function getYearOfBirth(age) {
-  if (age < 0) {
-    throw new Error('Age cannot be negative.');
-  }
   return 2019 - age;
 }
 
 try {
   const greet = createGreeting('Brittany', -3);
 } catch (e) {
-  console.log(e);
-};
+  console.log(e.message);
+}
 
 function createGreeting(name, age) {
+  if (age < 0) {
+    throw new Error('Age cannot be negative.');
+  }
+
   if (!name || !age) {
     throw new Error('Arguments not valid.');
   }
